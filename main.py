dacbef24bbbd6fe77c7b125042162c13511425dd
Lynn <48946984+Lynnsanee@users.noreply.github.com>
2022-06-24 11:31:28 +0200

    remove unused imports

--- main.py
+++ main.py
@@ -7,10 +7,7 @@ import logging
 # telegram
 from aiogram import Bot, Dispatcher, executor, types
 
-# utilities
-import asyncio, datetime, time
-
-#import of local files
+# import of local files
 import credentials
 import lib as F
 
@@ -36,16 +33,19 @@ async def plainMessageHandling(message: types.Message):
             # await the functions from the library file, pass the bot oject and the message, 
             # in addition to any other params you might need, like commandText or commandArguments
             await F.startMessage(bot, message, other_args)
+            
     # forwards
     if message.forward_from:
         if message.forward_from.username == 'Lynnsane' and 'Example Forward' in message.text:
             await F.functionsExample(bot, message, other, args)
 
+            
 # handle new joins here
 @dp.message_handler(content_types=types.ContentTypes.NEW_CHAT_MEMBERS)
 async def new_chat_member(message: types.Message):
     print(message)
 
+    
 if __name__ == "__main__":
     # start the dispatcher here
     executor.start_polling(dp)
